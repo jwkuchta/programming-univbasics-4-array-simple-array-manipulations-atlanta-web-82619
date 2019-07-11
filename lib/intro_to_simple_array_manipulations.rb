58a6a4c94096cd488ebcfc33932cf75614e65704
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-11 23:13:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,10 +14,34 @@ def pop_with_args(array)
   return array.pop(2)
 end
 
-def shift(array)
+def using_shift(array)
   return array.shift()
 end
 
 def shift_with_args(array)
   return array.shift(-1, 2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, new_element)
+  array.insert[4](new_element)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
 end
